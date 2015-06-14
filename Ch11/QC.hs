3c3048c06106e8042299060488745d5b6d6f2bfb
Dr-Lord <Lord.Fletcher@outlook.com>
2015-06-14 23:58:09 +0100

    Picked up Chapter 11

--- Ch11/QC.hs
+++ Ch11/QC.hs
@@ -1,7 +1,18 @@
 -- File: Real World Haskell\Ch11\QC.hs
 import Arbitrary
+import Prettify2
+import Test.QuickCheck
 
 prop_empty_id x =
     empty <> x == x
   &&
     x <> empty == x
+
+
+prop_char c   = char c   == Char c
+
+prop_text s   = text s   == if null s then Empty else Text s
+
+prop_line     = line     == Line
+
+prop_double d = double d == text (show d)
